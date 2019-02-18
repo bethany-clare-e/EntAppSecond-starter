502d64183eee361a984171ceec196d47fc599947
Bethany Endersby <S00190486@mail.itsligo.ie>
2019-02-18 21:25:58 +0000

    Add project files.

---
+++ EntAppSecond/Pages/Student/Create.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EntAppSecond.Pages.stud
+{
+    public class CreateModel : PageModel
+    {
+        [BindProperty]
+
+        public Student Student { get; set; }
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ EntAppSecond/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntAppSecond
+{
+    public class Student
+    {
+        [Required(ErrorMessage = "Please enter a student ID")]
+        public string StudentID { get; set; } = "";
+        [Required]
+        public string FirstName { get; set; } = "";
+        //[Display(Name = "First Name")]
+        [Required]
+        public string LastName { get; set; } = "";
+    }
+}
+
