fb732fdeb2bfb40921f521c9c8d2e9b7b10f4011
Bethany Endersby <S00190486@mail.itsligo.ie>
2019-02-21 18:26:56 +0000

    validation ws1 and ws2

---
+++ EntAppSecond/Model/Student.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntAppSecond.Model
+{
+    public class Student
+    {
+            [Display(Name = "Student ID")]
+            [RegularExpression(@"^[sS][0-9]{8}$")]
+            [StringLength(9)]
+            [Required(ErrorMessage = "Please enter a student ID")]
+            public string StudentID { get; set; } = "";
+
+
+            [Display(Name = "First Name")]
+            [StringLength(30,MinimumLength =2)]
+            [Required]
+            public string FirstName { get; set; } = "";
+
+
+            [Display(Name = "Surname")]
+            [StringLength(30,MinimumLength =3)]
+            [Required]
+            public string LastName { get; set; } = "";
+
+
+            [DataType(DataType.Date)]
+            [Display(Name = "Date of Birth")]
+            [Required]
+            public string DOB { get; set; } 
+
+
+            [Display(Name = "Number of Modules")]
+            [Required]
+            public int NumberOfModules { get; set; }
+
+
+            [Display(Name = "Height(cm)")]
+            [Required]
+            public int HeightInCM { get; set; }
+
+
+            [DataType(DataType.EmailAddress)]
+            [Display(Name = "Email Address")]
+            [Required]
+            public string Email { get; set; }
+
+
+            [DataType(DataType.EmailAddress)]
+            [Display(Name ="Confirm Email Address")]
+            [Required]
+            public string EmailConfirm { get; set; }
+
+    }
+}
--- EntAppSecond/Pages/Student/Create.cshtml.cs
+++ EntAppSecond/Pages/Student/Create.cshtml.cs
@@ -11,10 +11,30 @@ namespace EntAppSecond.Pages.stud
     {
         [BindProperty]
 
-        public Student Student { get; set; }
+        public Model.Student Student { get; set; }
         public void OnGet()
         {
 
+
+        }
+
+        public void OnPost()
+        {
+            if (Student.NumberOfModules <= 1)
+            {
+                ModelState.AddModelError("Student.NumberOfModules", "You must take more than 1 module");
+            }
+
+            if (Student.HeightInCM < 50 || Student.HeightInCM >= 250)
+            {
+                ModelState.AddModelError("Student.HeightInCM", "Height must be < 50 cm and > 250cm");
+                    
+            }
+
+            if (Student.Email != Student.EmailConfirm)
+            {
+                ModelState.AddModelError("Student.Email", "Emails don't match");
+            }
         }
     }
 }
